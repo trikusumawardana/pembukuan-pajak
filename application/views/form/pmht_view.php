98c5d0de6d5bcea1ef70082208edb49bff78977e
Tri Kusumawardana <trikusumawardana6@gmail.com>
2025-03-06 14:33:31 +0700

    buat demo halaman phmt

--- application/views/form/pmht_view.php
+++ application/views/form/pmht_view.php
@@ -1,16 +1,53 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman PH-MT</title>
     <link href="<?= base_url('assets/css/css-form.css') ?>" rel="stylesheet">
 </head>
+
 <body>
-    <div class="container">
-        <h1>Halaman PH-MT</h1>
-        <p>Ini adalah halaman khusus untuk status kewajiban perpajakan PH atau MT.</p>
-        <a href="<?= site_url('form') ?>" class="btn-grey">Kembali ke Form</a>
+
+    <button type="submit" form="form-identitas" name="action" value="sebelumnya" class="btn-grey">SEBELUMNYA</button>
+    </div>
+    <div class="form-center">
+        <h2>LEMBAR PERHITUNGAN PAJAK PENGHASILAN TERUTANG <br>
+            BAGI WAJIB PAJAK YANG KAWIN DENGAN STATUS PERPAJAKAN SUAMI-ISTERI PISAH HARTA DAN PENGHASILAN (PH) ATAU <br>
+            ISTERI YANG MENGHENDAKI UNTUK MENJALANKAN HAK DAN KEWAJIBAN PERPAJAKANNYA SENDIRI (MT)</h2>
+    </div>
+
+    <div class="section-container">
+        <table class="income-table" border="1px">
+            <tbody>
+                <tr>
+                    <th>1</th>
+                    <th>2</th>
+                    <th>3</th>
+                    <th>4</th>
+                    <th>5</th>
+                    <th>6</th>
+                </tr>
+                <tr>
+                    <td>1a</td>
+                    <td>2a</td>
+                    <td>3a</td>
+                    <td>4a</td>
+                    <td>5a</td>
+                    <td>6a</td>
+                </tr>
+                <tr>
+                    <td>1b</td>
+                    <td>2b</td>
+                    <td>3b</td>
+                    <td>4b</td>
+                    <td>5b</td>
+                    <td>6b</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
 </body>
+
 </html>
